01e75efdfedd92aed1fb18cf757a8762dd4b581b
Soham-2411 <soham.s2411@gmail.com>
2025-09-17 11:51:31 -0400

    Update email link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     const socialLinks = [
         {
             name: 'Email',
-            url: 'mailto:sohamsakaria@gmail.com',
+            url: 'https://mail.google.com/mail/?view=cm&fs=1&to=sohamsakaria@gmail.com',
             icon: (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
